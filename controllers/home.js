c45ceb9cb7246b2bf9baaa719f67de65c9bbf381
Cory Simpson <cory.simpson.dev@gmail.com>
2022-09-02 11:53:19 -0700

    added country list for modal - requires package

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,12 @@
 const Checkin = require('../models/Checkin')
+const countryList = require('country-list')
 
 module.exports = {
     getIndex: async (req,res)=>{
         console.log(req.user)
         try{
             const posts = await Checkin.find({status:"public"})
-            res.render('index.ejs', {title: 'Tastes of the Town', posts: posts, user: req.user})
+            res.render('index.ejs', {title: 'Tastes of the Town', posts: posts, user: req.user, countryData: countryList.getData()})
         }catch(err){
             console.log(err)
         }
